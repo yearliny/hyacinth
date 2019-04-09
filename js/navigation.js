3b4385124a47b355ca513d9fa2a5b2af204b1fd1
yearliny <yearliny@outlook.com>
2019-04-09 21:13:20 +0800

    优化

--- js/navigation.js
+++ js/navigation.js
@@ -1,55 +1,37 @@
-// auto hide navbar
-// let previousTop = 0;
+/**
+ * 导航栏自动隐藏
+ */
+"use strict";
+class auto_nav {
 
-function auto_nav() {
-    let header = document.getElementById("site-navigation");
-    let headerHeight = header.offsetHeight;
-
-    // 监听滚动事件
-    let currentTop = window.scrollY;
-    // 如果向上滚动
-    if (currentTop < previousTop) {
-        if (currentTop > 0 && header.classList.contains("is-fixed")) {
-            header.classList.add("is-visible");
-        } else {
-            header.classList.remove("is-visible", "is-fixed")
-        }
-    } else if (currentTop > previousTop) {
-        header.classList.remove("is-visible");
-        if (currentTop > headerHeight && !header.classList.contains("is-fixed")) {
-            header.classList.add("is-fixed");
-        }
+    constructor(){
+        this.header = document.getElementById("site-navigation");
+        this.headerHeight = this.header.offsetHeight;
+        // 设定默认值
+        this.previousTop = 0;
     }
-    previousTop = currentTop;
+
+    handleEvent() {
+        // 监听滚动事件
+        const currentTop = window.scrollY;
+        // 如果向上滚动
+        if (currentTop < this.previousTop) {
+            if (currentTop > 0 && this.header.classList.contains("is-fixed")) {
+                this.header.classList.add("is-visible");
+            } else {
+                this.header.classList.remove("is-visible", "is-fixed")
+            }
+        } else if (currentTop > this.previousTop) {
+            this.header.classList.remove("is-visible");
+            if (currentTop > this.headerHeight && !this.header.classList.contains("is-fixed")) {
+                this.header.classList.add("is-fixed");
+            }
+        }
+        this.previousTop = currentTop;
+    }
 }
-
-let auto_nav_bate = function () {
-    this.header = document.getElementById("site-navigation");
-    this.headerHeight = this.header.offsetHeight;
-    // 设定默认值
-    this.previousTop = 0;
-};
-
-auto_nav_bate.prototype.handleEvent = function () {
-    // 监听滚动事件
-    let currentTop = window.scrollY;
-    // 如果向上滚动
-    if (currentTop < this.previousTop) {
-        if (currentTop > 0 && this.header.classList.contains("is-fixed")) {
-            this.header.classList.add("is-visible");
-        } else {
-            this.header.classList.remove("is-visible", "is-fixed")
-        }
-    } else if (currentTop > this.previousTop) {
-        this.header.classList.remove("is-visible");
-        if (currentTop > this.headerHeight && !this.header.classList.contains("is-fixed")) {
-            this.header.classList.add("is-fixed");
-        }
-    }
-    this.previousTop = currentTop;
-};
 
 // 只有在屏幕宽度大于 992，grid-lg(桌面设备) 宽度的情况下，才会进行自动隐藏 bar
 if (screen.width > 992) {
-    window.addEventListener("scroll", new auto_nav_bate());
+    window.addEventListener("scroll", new auto_nav());
 }
